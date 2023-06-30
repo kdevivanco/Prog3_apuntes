eb9249d31137b24c8f93390b81a00b625049ae5e
kdevivanco <kdevivanco@gmail.com>
2023-06-30 08:56:50 -0500

    arbol binario c busqueda

--- semana_14/arbol_binario.cpp
+++ semana_14/arbol_binario.cpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <memory>
 #include <queue>
+#include <string>
 
 
 using namespace std;
@@ -12,36 +13,48 @@ using namespace std;
 
 //Create binary tree starting with nodes
 template<typename K, typename V, typename Compare = std::less<K>>
-        class binary_search_tree{
+        class binary_search_tree {
             struct Node;
             using item_t = pair<K, V>;
-            using shared_ptr = shared_ptr<Node>;
+            using shared_ptr_t = shared_ptr<Node>;
+            using weak_ptr_t = weak_ptr<Node>;
 
-            struct Node{
+            struct Node {
                 pair<K, V> item;
                 shared_ptr<Node> left;
                 shared_ptr<Node> right;
                 weak_ptr<Node> parent;
 
-               Node(pair<K, V> item, weak_ptr<Node> parent):
-                item(item), parent(parent){}
+                Node(item_t item, shared_ptr_t parent) :
+                        item(item), parent(parent) {}
 
             };
 
-            shared_ptr root;
+            shared_ptr_t root;
             Compare cmp;
+
+            template<typename Callable>
+            void _in_order(Node *node, Callable& fun) {
+                    if(node == nullptr){
+                        return;
+                    }
+                    _in_order(node->left.get(), fun);
+                    fun(node->item);
+                    _in_order(node->right.get(), fun);
+            }
+
+
         public:
             binary_search_tree() = default;
             //insert
-            void insert(const item_t& item) {
-                if (!root) {
+            void insert(item_t item) {
+                if (root == nullptr) {
                     root = make_shared<Node>(item, nullptr);
                 }
                 auto current = root;
-
                 while(current){ //existe
                     if(cmp(item.first, current->item.first)){
-                        if(current->left == false){ // si no existe ya un valor a la izquierda
+                        if(!current->left){ // si no existe ya un valor a la izquierda
                             current->left = make_shared<Node>(item, current);
                             break; //para terminar el while
                         }
@@ -73,7 +86,7 @@ template<typename K, typename V, typename Compare = std::less<K>>
                 while(!q.empty()){
                     //Obtener front
                     auto current = q.front();
-                    fun(current);
+                    fun(current->item);
                     //Realizar pop
                     q.pop();
                     //Agregar todos los hijos al queue del nodo extraido
@@ -84,5 +97,31 @@ template<typename K, typename V, typename Compare = std::less<K>>
                 }
             }
 
+            template<typename Callable>
+            void in_order(const Callable& fun) {
+                _in_order(root.get(),fun);
+
+            }
 
         };
+
+int main(){
+    binary_search_tree<int,string> bst;
+    bst.insert({40,"B"});
+    bst.insert({20,"A"});
+    bst.insert({10,"A"});
+    bst.insert({30,"A"});
+    bst.insert({50,"A"});
+    bst.insert({60,"A"});
+
+    bst.in_order([](auto item){
+        cout << item.first << "  " << item.second << endl;
+    });
+
+    cout << "BFS" << endl;
+    bst.bfs([](auto item){
+        cout << item.first << "  " << item.second << endl;
+    });
+
+    return 0;
+}
